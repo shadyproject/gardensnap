624be54fae727fc4f85f9b78da663c62ebc0084d
Shady <shady@shadyproject.net>
2013-11-23 12:42:36 -0800

    added rooot view controller

---
+++ GardenSnap/ViewControllers/GSRootViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSRootViewController.h
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSRootViewController : UITableViewController
+
+@end
