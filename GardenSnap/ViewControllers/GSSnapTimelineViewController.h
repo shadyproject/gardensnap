eaf45f220f13182ecad0a1e12f7be0220705edea
Shady <shady@shadyproject.net>
2013-11-23 13:13:50 -0800

    Timeline view controller

---
+++ GardenSnap/ViewControllers/GSSnapTimelineViewController.h
@@ -0,0 +1,13 @@
+//
+//  GSSnapTimelineViewController.h
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSSnapTimelineViewController : UICollectionViewController
+
+@end
---
+++ GardenSnap/ViewControllers/GSSnapTimelineViewController.m
@@ -0,0 +1,38 @@
+//
+//  GSSnapTimelineViewController.m
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import "GSSnapTimelineViewController.h"
+
+@interface GSSnapTimelineViewController ()
+
+@end
+
+@implementation GSSnapTimelineViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
