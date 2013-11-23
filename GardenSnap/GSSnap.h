caa826dffd41f3044ec692cdb93683bea40e9c62
Shady <shady@shadyproject.net>
2013-11-23 13:50:06 -0800

    snap object and big placeholder

---
+++ GardenSnap/GSSnap.h
@@ -0,0 +1,13 @@
+//
+//  GSSnap.h
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSSnap : NSObject
+
+@end
---
+++ GardenSnap/GSSnap.m
@@ -0,0 +1,13 @@
+//
+//  GSSnap.m
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import "GSSnap.h"
+
+@implementation GSSnap
+
+@end
