75cbc405dcd057d83e2bde64dac2bc5bd30b250c
Shady <shady@shadyproject.net>
2013-11-23 13:51:50 -0800

    create controller to make new naps

---
+++ GardenSnap/GSSnapCreatorController.h
@@ -0,0 +1,13 @@
+//
+//  GSSnapCreatorController.h
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GSSnapCreatorController : NSObject
+
+@end
---
+++ GardenSnap/GSSnapCreatorController.m
@@ -0,0 +1,13 @@
+//
+//  GSSnapCreatorController.m
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import "GSSnapCreatorController.h"
+
+@implementation GSSnapCreatorController
+
+@end
