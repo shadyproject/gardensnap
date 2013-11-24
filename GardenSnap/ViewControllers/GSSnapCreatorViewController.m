3335acac9133f3b99c41f558171b2e03d521bf10
Shady <shady@shadyproject.net>
2013-11-23 17:45:09 -0800

    Single view controller for pics and editing

---
+++ GardenSnap/ViewControllers/GSSnapCreatorViewController.m
@@ -0,0 +1,38 @@
+//
+//  GSSnapCreatorViewController.m
+//  GardenSnap
+//
+//  Created by Christopher Martin on 11/23/13.
+//  Copyright (c) 2013 GardenSnap. All rights reserved.
+//
+
+#import "GSSnapCreatorViewController.h"
+
+@interface GSSnapCreatorViewController ()
+
+@end
+
+@implementation GSSnapCreatorViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
